4e0470e8a1f1f886373eb4ad7f5e0c251cae5646
hristokoev <hkoev@DESKTOP-49TMISQ.localdomain>
2022-12-03 15:27:29 +0100

    c00/ex07

---
+++ c00/ft_putnbr.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+       	if (nb == -2147483648)
+        {
+            ft_putnbr(-2);
+            ft_putnbr(147483648);
+            return;
+        }
+        if (nb >= 0 && nb <= 9)
+        {
+            ft_putchar(nb + '0');
+        }
+        else if (nb < 0)
+        {
+            ft_putchar('-');
+            ft_putnbr(nb * -1);
+        }
+        else
+        {
+            ft_putnbr(nb / 10);
+            ft_putnbr(nb % 10);
+        }
+
+}
