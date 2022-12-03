5104b6317a72b76c01d25fef63e07432dea4da68
hristokoev <hkoev@DESKTOP-49TMISQ.localdomain>
2022-12-03 15:26:23 +0100

    Finished c00/ex06

--- c00/ft_print_comb2.c
+++ c00/ft_print_comb2.c
@@ -1,12 +1,30 @@
 #include <unistd.h>
 
-void	ft_print_comb2(void)
+void	ft_putchar(char c)
 {
-
+	write(1, &c, 1);
 }
 
-int	main(void)
+void	ft_print_comb2(void)
 {
-	ft_print_comb2();
-	return (0);
+	int		a;
+	int		b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
+			write(1, " ", 1);
+			ft_putchar((b / 10) + '0');
+			ft_putchar((b % 10) + '0');
+			if (a != 98 || b != 99)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}	
 }
