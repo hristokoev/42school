a1e06348383f5c6e6b54806a048a521cddbd0f01
hristokoev <hkoev@DESKTOP-49TMISQ.localdomain>
2022-12-28 23:14:42 +0100

    Added header

--- c00/ft_putnbr.c
+++ c00/ft_putnbr.c
@@ -1,31 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkoev <hkoev@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 13:43:18 by hkoev             #+#    #+#             */
+/*   Updated: 2022/12/04 13:43:19 by hkoev            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-       	if (nb == -2147483648)
-        {
-            ft_putnbr(-2);
-            ft_putnbr(147483648);
-            return;
-        }
-        if (nb >= 0 && nb <= 9)
-        {
-            ft_putchar(nb + '0');
-        }
-        else if (nb < 0)
-        {
-            ft_putchar('-');
-            ft_putnbr(nb * -1);
-        }
-        else
-        {
-            ft_putnbr(nb / 10);
-            ft_putnbr(nb % 10);
-        }
-
+	if (nb == -2147483648)
+	{
+		ft_putnbr(-2);
+		ft_putnbr(147483648);
+		return ;
+	}
+	if (nb >= 0 && nb <= 9)
+	{
+		ft_putchar(nb + '0');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
 }
